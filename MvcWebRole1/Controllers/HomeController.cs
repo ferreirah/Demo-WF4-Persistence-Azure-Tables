109f24fb37f5cb9dc53e96a5be38475c1c112b6b
Hugo Ferreira <ferreirah@msn.com>
2013-07-17 17:26:49 +0100

    Missing files added.

---
+++ MvcWebRole1/Controllers/HomeController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Microsoft.WindowsAzure;
+using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure.Storage.Auth;
+using Microsoft.WindowsAzure.Storage.Queue;
+
+
+namespace MvcWebRole1.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            CloudStorageAccount storageAccount = CloudStorageAccount.DevelopmentStorageAccount;
+
+            CloudQueueClient queueClient = storageAccount.CreateCloudQueueClient();
+            
+            CloudQueue queue = queueClient.GetQueueReference("batatinhas");
+            queue.CreateIfNotExists();
+            queue.AddMessage(new CloudQueueMessage("Hello world!"));
+            
+            return View();
+        }
+    }
+}
---
+++ MvcWebRole1/Controllers/WorkflowConfigController.cs
@@ -0,0 +1,51 @@
+﻿using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure.Storage.Queue;
+using Newtonsoft.Json.Linq;
+
+namespace MvcWebRole1.Controllers
+{
+    public class WorkflowConfigController : ApiController
+    {
+        private CloudStorageAccount account;
+        private CloudQueueClient queueClient;
+        private CloudQueue queue;
+
+        public WorkflowConfigController()
+        {
+            account = CloudStorageAccount.DevelopmentStorageAccount;
+            queueClient = account.CreateCloudQueueClient();
+            queue = queueClient.GetQueueReference("batatinhas");
+            queue.CreateIfNotExists();
+        }
+
+        public HttpResponseMessage Post()
+        {
+            queue.AddMessage(new CloudQueueMessage("Hello World!"));
+
+            return new HttpResponseMessage(HttpStatusCode.Accepted);
+        }
+
+        public HttpResponseMessage Get()
+        {
+            var messages = queue.PeekMessages(500);
+            JObject obj = new JObject();
+            JArray array = new JArray();
+
+            obj.Add("messages", array);
+
+            foreach (var _message in messages)
+            {
+                JObject jsonMessage = new JObject();
+                jsonMessage.Add("id", _message.Id);
+                jsonMessage.Add("content", _message.AsString);
+                jsonMessage.Add("expiration_time", _message.ExpirationTime);
+                array.Add(jsonMessage);
+            }
+
+            return Request.CreateResponse(HttpStatusCode.OK, obj);
+        }
+    }
+}
